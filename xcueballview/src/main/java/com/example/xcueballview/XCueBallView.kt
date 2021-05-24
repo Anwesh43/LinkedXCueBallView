ef2a63315a7f84c0bb988cbf73aece2c3cf74aa2
Anwesh43 <anweshthecool0@gmail.com>
2021-05-25 01:21:52 +0530

    adding constants and libraries

--- xcueballview/src/main/java/com/example/xcueballview/XCueBallView.kt
+++ xcueballview/src/main/java/com/example/xcueballview/XCueBallView.kt
@@ -1,2 +1,24 @@
 package com.example.xcueballview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#311B92",
+    "#00C853",
+    "#00C853",
+    "#C51162"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val strokeFactor : Float = 90f
+val rFactor : Float = 6.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
