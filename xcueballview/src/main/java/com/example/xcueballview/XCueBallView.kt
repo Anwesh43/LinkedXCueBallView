019e00050f870ebe964d5675d5bdb59eaa5ae109
Anwesh43 <anweshthecool0@gmail.com>
2021-05-25 01:34:33 +0530

    adding State class

--- xcueballview/src/main/java/com/example/xcueballview/XCueBallView.kt
+++ xcueballview/src/main/java/com/example/xcueballview/XCueBallView.kt
@@ -22,6 +22,8 @@ val strokeFactor : Float = 90f
 val rFactor : Float = 6.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val scGap : Float = 0.02f / parts
+
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -74,5 +76,25 @@ class XCueBallView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
