fceba5130b9107ff0d51a2d02a5501729c66ade6
Anwesh43 <anweshthecool0@gmail.com>
2021-05-25 01:54:05 +0530

    adding static create method

--- xcueballview/src/main/java/com/example/xcueballview/XCueBallView.kt
+++ xcueballview/src/main/java/com/example/xcueballview/XCueBallView.kt
@@ -214,4 +214,13 @@ class XCueBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : XCueBallView {
+            val view : XCueBallView = XCueBallView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
