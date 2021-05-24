7f8343fbeeb310b473c05a2fee0838ab58751ecf
Anwesh43 <anweshthecool0@gmail.com>
2021-05-25 01:41:40 +0530

    adding XCueBall class

--- xcueballview/src/main/java/com/example/xcueballview/XCueBallView.kt
+++ xcueballview/src/main/java/com/example/xcueballview/XCueBallView.kt
@@ -166,4 +166,27 @@ class XCueBallView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class XCueBall(var i : Int) {
+
+        private var curr : XCBNode = XCBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
